ae214bfc109ac8d3791f07a08e9ce751cda8cb2a
lzif <lukyfriendly@gmail.com>
2024-12-09 14:01:53 +0700

    feat(db): support connecting to xata via environment variables
    The change allows specifying the Xata database URL, API key, and branch name via environment variables. This makes it possible to connect to different Xata databases or branches without changing the code and improves the security by not hardcoding sensitive information.

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,27 @@
 import { Kysely } from "kysely";
 import { XataDialect, type Model } from "@xata.io/kysely";
-import { type DatabaseSchema, getXataClient } from "./xata";
-const xata = getXataClient();
+import { XataClient, type DatabaseSchema } from "./xata";
+import {
+  XATA_DATABASE_URL,
+  XATA_API_KEY,
+  XATA_BRANCH,
+} from "$env/static/private";
+
+let instance: XataClient | undefined = undefined;
+
+export const getXataClient = () => {
+  if (instance) return instance;
+
+  instance = new XataClient({
+    databaseURL: XATA_DATABASE_URL,
+    apiKey: XATA_API_KEY,
+    branch: XATA_BRANCH,
+  });
+  return instance;
+};
 
 const db = new Kysely<Model<DatabaseSchema>>({
-  dialect: new XataDialect({ xata }),
+  dialect: new XataDialect({ xata: getXataClient() }),
 });
 
 export default db;
